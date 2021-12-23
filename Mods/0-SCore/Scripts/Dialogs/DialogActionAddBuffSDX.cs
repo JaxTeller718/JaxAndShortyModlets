52a44803bccf2cc7358350d6252369e938883c47
Jax <kingsoprano718@gmail.com>
2021-12-22 21:00:53 -0500

    update

--- Mods/0-SCore/Scripts/Dialogs/DialogActionAddBuffSDX.cs
+++
@@ -1,7 +0,0 @@
-﻿public class DialogActionAddBuffSDX : DialogActionAddBuff
-{
-    public override void PerformAction(EntityPlayer player)
-    {
-        player.Buffs.AddBuff(base.ID, -1, true);
-    }
-}
